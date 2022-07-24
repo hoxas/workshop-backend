742161855ceb94014ba5b691025d15a122b29f16
hoxas <hoxas@live.com>
2022-07-24 16:17:53 -0300

    Completando desafio

--- calculadora/js/desafio.js
+++ calculadora/js/desafio.js
@@ -1,11 +1,23 @@
 const inputValorHora = document.querySelector("#valor-hora");
 const inputHorasProjeto = document.querySelector("#horas-projeto");
 const resposta = document.querySelector("#resposta");
+const botaoCalcular = document.querySelector("#botao-calcular");
+
+const resultDiv = document.querySelector("#result");
 
 const calcular = () => {
     const valorHora = Number(inputValorHora.value);
     const horasProjeto = Number(inputHorasProjeto.value);
     const calculo = valorHora * horasProjeto;
+    animar(botaoCalcular, "animate__headShake");
+    animar(resultDiv, "animate__flash");
     const resultado = "R$ " + calculo.toFixed(2).replace(".", ",");
-    resposta.innerHTML = `Esse projeto custará: ${resultado}`;
+    resposta.innerHTML = resultado;
+};
+
+const animar = (elementoHtml, classeAnimacao) => {
+    elementoHtml.classList.add("animate__animated", classeAnimacao);
+    setTimeout(() => {
+        elementoHtml.classList.remove("animate__animated", classeAnimacao);
+    }, 1000);
 };
